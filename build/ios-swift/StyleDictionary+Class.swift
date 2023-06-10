928972efcc5e026e7074b710c3589ea248e56490
Matt Trice <matt@trice.design>
2023-06-09 22:52:31 -0400

    change color to green, add description to token

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,12 +4,12 @@
 //
 
 // Do not edit directly
-// Generated on Sat, 10 Jun 2023 01:18:55 GMT
+// Generated on Sat, 10 Jun 2023 02:46:56 GMT
 
 
 import UIKit
 
 public class StyleDictionaryClass {
-    public static let bg = #FF005C
+    public static let bg = #14ff00
     public static let tokenSetOrder0 = Big Set
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,12 +4,12 @@
 //
 
 // Do not edit directly
-// Generated on Sat, 10 Jun 2023 01:18:55 GMT
+// Generated on Sat, 10 Jun 2023 02:46:56 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let bg = #FF005C
+    internal static let bg = #14ff00
     internal static let tokenSetOrder0 = Big Set
 }
